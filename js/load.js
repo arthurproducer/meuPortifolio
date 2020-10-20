1ce5c8a814ebdf6044ddadcc41ca73dd26eaf34a
arthurproducer <arthurproducer@gmail.com>
2020-10-20 01:13:30 -0300

    refactor skills, historic, footer and about

--- js/load.js
+++ js/load.js
@@ -2,38 +2,3 @@ const load = () => {
     const backToTop = new BackToTop();
     console.log('Texto de carregamento');
 }
-
-async function enviar(){
-    alert(1);
-    const name = document.querySelector('.name').value; 
-    const subject = document.querySelector('.subject').value;
-    const email = document.querySelector('.email').value;
-    const message = document.querySelector('.message').value;
-
-    const msg = {
-        from: "arthur.sales@assessoria.app",
-        to: 'arthur.smii@hotmail.com',
-        subject: "Assunto do Heroku",
-        text: `
-            Nome de quem entrou em contato: ${name}
-            Email de quem entrou em contato: ${email}
-            Messagem: ${message}
-            `
-    };
-
-    try{
-    const data = await fetch('https://fiap-sender.herokuapp.com/email/send',
-    {
-        method: 'POST',
-        headers:{
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(msg),
-    }
-    )
-
-    }catch(err){
-        console.error(err);
-    } 
-}
--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,43 +1,39 @@
 /**
  * @name "sendEmail" - Responsável por enviar Email
  */
-function sendEmail() {
-    const loading = document.querySelector('.loading');
 
-    loading.className = loading.className + ' show';
-
-    const name = document.querySelector('.name').value;
+async function sendEmail(){
+    alert("E-mail enviado com sucesso!");
+    const name = document.querySelector('.name').value; 
     const subject = document.querySelector('.subject').value;
     const email = document.querySelector('.email').value;
     const message = document.querySelector('.message').value;
 
     const msg = {
-        from: "gustavocaspirro@assessoria.app",
-        to: "caspirrogustavo@gmail.com",
-        subject: subject,
+        from: "arthur.sales@assessoria.app",
+        to: 'arthur.smii@hotmail.com',
+        subject: "Assunto do Heroku",
         text: `
             Nome de quem entrou em contato: ${name}
             Email de quem entrou em contato: ${email}
-
-            Mensagem: ${message}
-        `
+            Messagem: ${message}
+            `
     };
 
-    fetch('https://fiap-sender.herokuapp.com/email/send',
-        {
-             method: 'POST',
-             headers: {
-                 'Accept': 'application/json',
-                 'Content-Type': 'application/json'
-             },
-            body: JSON.stringify(msg),
-        }
+    try{
+    const data = await fetch('https://fiap-sender.herokuapp.com/email/send',
+    {
+        method: 'POST',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(msg),
+    }
     )
-    .then(data => {
-           loading.className = loading.className.replace('show', '');
-           alert("E-mail enviado com sucesso");
-    }).catch(err => {
-        loading.className = loading.className.replace('show', '');
-        console.error(`Mensagem de erro ${err}`);
-    });
+
+    }catch(err){
+        alert(err);
+        console.error(err);
+    } 
 }
